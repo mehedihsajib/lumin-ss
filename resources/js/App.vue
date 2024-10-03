a050fef7a0ba875722050be28498debfea4b6123
Mehedi Hasan <sajibmehedi7@gmail.com>
2024-10-03 10:31:38 +0600

    project init

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1 class="text-4xl text-green-400">Hello, Vue + Laravel!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+  
